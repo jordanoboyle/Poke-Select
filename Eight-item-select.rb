f3773afffcb76bcbe3f8c954c18dafb21645a327
jordanoboyle <jmcmcode@gmail.com>
2024-05-16 14:55:45 -0600

    developing tty-prompt know how. Can assign choices to vars and use, Eight-item-select

---
+++ Eight-item-select.rb
@@ -0,0 +1,12 @@
+require "tty-prompt"
+
+# quick overview of %w flag for the uninitiated (ie: ME).
+# %w() is a shortcut or shorthand way of creating an array in ruby without the need for commas and quotes. Each word/line-of-characters is converted into a string element in an array. Example below:
+fruits = %w(banana apple pear strawberry asldfasdf)
+p fruits # see NEAT
+
+prompt = TTY::Prompt.new
+
+choices = %w(Eggmoves EvolutionChain MoveSet LevelEvolve ShinyColor HMLearnables Type Weaknesses Strengths)
+user_choice = prompt.multi_select("Select what you would like to know more about?", choices)
+p "DefaultName would like to know more about #{user_choice[0]}."
--- test-selector.rb
+++ test-selector.rb
@@ -4,3 +4,20 @@ require "tty-prompt"
 # let's figure out how to use this!!!!
 prompt = TTY::Prompt.new
 
+# prompt.ask("What is you name?", default: ENV["USER"])  # this pulls user from zsh terminal environment
+
+# prompt.yes?("Do you like using ruby?") # this will prompt for yes or no
+
+# prompt.mask("What is your secret?") # This will hide the answer....dive into docs for this.
+
+first_choice = prompt.select("Whos that pokemon?", %w(Squirtle Charmander Bulbasaur))  # provides a selector, choice can be assigned to a user_input variable
+p first_choice
+
+# ASK A MULTIPLE CHOICE QUESTION
+choices = %w(Pikachu Vaporeon Umbreon Cinderace)
+fav_poke = prompt.multi_select("Of these pokemon, which is your favorite?", choices)
+p fav_poke  #follow the instructions you dummy. Use "SPACEBAR" assigns to var as array
+p fav_poke[0]
+
+
+
