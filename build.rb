8e5b921ffd2049eb2e2ab5bb9d1349692db4160d
jordanoboyle <jmcmcode@gmail.com>
2024-05-21 01:25:01 -0600

    should have committed ages ago

--- build.rb
+++ build.rb
@@ -14,15 +14,6 @@ def choose_pokemon
     return name_array
   end
 
-  # p fetch_pokemon
-
-  #goal: fetch a random selection of pokemon from kanto and create a selector with "'pokemon name', I choose you!"
-  #create a random number generator for numbers 0-10
-  # create array with no repeats of numbers
-  # expand this to make sure it can handle 151 pokemon, selection of 10 with no repeats.
-
-  # use this array to provide a selector with TTY-prompt
-  # when selected respond with response above!
   def random_ten
     random_ten = []
     while random_ten.length < 10
@@ -35,7 +26,7 @@ def choose_pokemon
     end
     return random_ten
   end
-
+  
   def poke_selector(rand_ten, poke_array)
     selector = []
     rand_ten.each do |n|
@@ -50,6 +41,33 @@ def choose_pokemon
 end
 
 choose_pokemon()
+
+class PokeAPI 
+  include HTTParty # this line includes all methods from HTTParty in this class
+
+  base_url = "https://pokeapi.co/api/v2/"
+
+  def get_pokemon_data(poke_name)
+    response = self.class.get("/pokemon/#{poke_name}")
+    poke_data = JSON.parse(response.body)
+  end
+
+  def get_species_data(poke_name)
+    response = HTTParty.get(
+    base_url + "/pokemon-species/#{poke_name}/"
+    )
+    species_data = JSON.parse(response.body)
+  end
+
+end
+
+#goal: fetch a random selection of pokemon from kanto and create a selector with "'pokemon name', I choose you!"
+#create a random number generator for numbers 0-10
+# create array with no repeats of numbers
+# expand this to make sure it can handle 151 pokemon, selection of 10 with no repeats.
+
+# use this array to provide a selector with TTY-prompt
+# when selected respond with response above!
 
 # make a method that allows the user to select from four different subjects they want to know about the pokemon they chose.
 #display that information and allow them to choose again if they like. 
--- getdatatest.rb
+++ getdatatest.rb
@@ -1,2 +1,68 @@
-require "http"
+require "httparty"
+require "json"
 
+
+
+base_url = 'https://pokeapi.co/api/v2'
+pokemon_name = "pikachu"
+
+# poke_data = HTTParty.get(
+#   base_url + "/pokemon/#{pokemon_name}"
+#   )
+# parsed_data = JSON.parse(poke_data.body)
+# p parsed_data.keys
+
+p "------------------
+---------------------"
+#get species data
+species_data = HTTParty.get(
+  base_url + "/pokemon-species/#{pokemon_name}/"
+  )
+parsed_data2 = JSON.parse(species_data.body)
+  
+  p "------------------
+  ---------------------"
+
+# HERE WE ARE ACCESSING ENCOUNTER DATA
+poke_data = HTTParty.get(
+  base_url + "/pokemon/#{pokemon_name}"
+  )
+parsed_data = JSON.parse(poke_data.body)
+
+
+encounters = HTTParty.get(
+  parsed_data["location_area_encounters"]
+)
+
+
+# GETS LOCATION DATA FROM RED AND BLUE
+# red_blue_encounters = []
+# i = 0
+# while i < encounters.length  # should convert to .select loop
+#   n = encounters[i]
+#   i2 = 0
+#   while i2 < n["version_details"].length
+#     vd = n["version_details"][i2]
+#     if ['red', 'blue'].include?(vd['version']['name']) == true
+#       red_blue_encounters << n
+#     end
+#     i2 += 1
+#   end
+#   i += 1
+# end
+
+# location_names = red_blue_encounters.map do |encounter| # GPT help
+#   encounter['location_area']['name']
+# end
+# p location_names.join(", ")
+
+
+
+#THIS GETS CAPTURE RATE NUMBER (OUT OF 255)
+# p parsed_data2["capture_rate"]
+
+# THIS GETS TYPES
+# types = parsed_data['types'].map do |t|
+#   t['type']['name']
+# end
+# puts "#{pokemon_name.capitalize}'s types are: #{types.join(', ')}"
